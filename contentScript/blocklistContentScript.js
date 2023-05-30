b381617254cecbd40aed5ff1c67e757f6b7db06d
HannahLeigh1997 <2582031777@qq.com>
2023-05-29 23:44:10 -0700

    pop up UI

--- contentScript/blocklistContentScript.js
+++ contentScript/blocklistContentScript.js
@@ -1,11 +1,49 @@
+// function blockSite() {
+//     document.body.innerHTML = '';
+//     document.body.style.backgroundColor = 'black';
+//     let blockMessage = document.createElement('h1');
+//     blockMessage.style.color = 'yellow';
+//     blockMessage.textContent = 'No way! You cannot access this site.';
+//     document.body.appendChild(blockMessage);
+// }
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request.command === "blockSite") {
+//         blockSite();
+//         console.log("site blocked");
+//     }
+// });
+
+// // Send a "ready" signal to the background script
+// chrome.runtime.sendMessage({command: "contentScriptReady"});
+   
+    
+      
+
 function blockSite() {
-    document.body.innerHTML = '';
-    document.body.style.backgroundColor = 'black';
-    let blockMessage = document.createElement('h1');
-    blockMessage.style.color = 'yellow';
-    blockMessage.textContent = 'No way! You cannot access this site.';
-    document.body.appendChild(blockMessage);
-}
+    let extensionUrl = chrome.runtime.getURL('contentScript/blocklist.html')
+   // document.body.innerHTML = generateHTML();
+    fetch(extensionUrl)
+        .then(response => response.text())
+        .then(html => {
+            document.body.innerHTML = html;
+        })
+        .catch(error => {
+            console.log('Error fetching blocklist:', error);
+        });
+} 
+//function blockSite() {
+//     document.body.innerHTML = '';
+//     document.body.style.backgroundColor = 'black';
+//     let blockMessage = document.createElement('h1');
+//     blockMessage.style.color = 'yellow';
+//     blockMessage.textContent = 'No way! You cannot access this site.';
+//     document.body.appendChild(blockMessage);
+       
+       //document.body.innerHTML = generateHTML();
+       //document.head.innerHTML = generateSTYLES();
+       //break;
+// }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.command === "blockSite") {
--- contentScript/youtubeContentScript.js
+++ contentScript/youtubeContentScript.js
@@ -7,12 +7,44 @@ function checkPage() {
         // the following code for blocking youtube elements are reused & modified from helloworld.js of the default template
         document.querySelector('ytd-browse[role="main"][page-subtype="home"] #contents').remove(); 
         document.querySelector('#guide-wrapper').remove();
-        var message = document.createElement("p");
-        message.textContent = "Hey, it's not the time to watch fun videos! A Leetcode a day, keeps rejection letter away.";
-        message.style = "font-size: 100px; color: orange; text-align: center;";
+        document.querySelector('#contentContainer').remove();
+        //document.querySelector('ytd-mini-guide-renderer #contents').remove();
+        var message1 = document.createElement("p");
+        message1.textContent = "Hey, it's not the time to watch fun videos! ";
+        message1.style = "font-size: 40px; color: orange; text-align: center;";
+        message1.style.textAlign = "center";
+        //message.style.backgroundColor = "skyblue";
+        //message.style.fontSize = "20px";
         var primaryElement = document.getElementById("primary");
-        primaryElement.appendChild(message);
-        
+        primaryElement.appendChild(message1);  
+
+        var message2 = document.createElement("p");
+        message2.textContent = "A Leetcode a day, keeps rejection letter away.";
+        message2.style = "font-size: 40px; color: orange; text-align: center;";
+        message2.style.textAlign = "center";
+        //message.style.backgroundColor = "skyblue";
+        //message.style.fontSize = "20px";
+        var primaryElement = document.getElementById("primary");
+        primaryElement.appendChild(message2);   
+        message2.style.marginLeft = "0px"; // 调整文字元素的左边距  
+        message2.style.marginBottom = "10px"; // 调整文字元素的底边距 
+
+        // Create an image element
+       var image = document.createElement("img");
+       image.src = "https://jella.tw/blog/wp-content/uploads/2021/01/%E5%B0%88%E6%B3%A8%E5%8A%9B.png"; // Replace with the actual path to your image
+       image.style = "display: block; margin: 0 auto;"; // Set image style for center alignment
+       image.style.width = "1100px";
+       image.style.height = "650px";
+       //image.style.margin = "0 auto";
+       //image.style.float = "center";
+       image.style.marginLeft = "0px"; // 调整图片元素的左边距   
+       
+  
+
+    
+    
+       // Append the image element to primaryElement
+       primaryElement.appendChild(image);
     }
 
 
